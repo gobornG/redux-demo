7bc0c2b35e3795b3c9c757105a8de7436c47dac9
Ladrillo <gabriel@cabrejas.net>
2017-10-12 17:49:53 -0600

    clean imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducer';
 
